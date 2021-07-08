125ea0d2d469a74c4c83996f84fc58fe307397d9
thisisommore <ommore501@gmail.com>
2021-07-08 10:09:09 +0000

    Use bionic instead of hirsute

--- .gitpod/qemu.sh
+++ .gitpod/qemu.sh
@@ -6,7 +6,7 @@ script_dirname="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 outdir="${script_dirname}/_output"
 
 sudo qemu-system-x86_64 -kernel "/boot/vmlinuz" \
--boot c -m 20049M -hda "${outdir}/rootfs/hirsute-server-cloudimg-amd64.img" \
+-boot c -m 20049M -hda "${outdir}/rootfs/bionic-server-cloudimg-amd64.img" \
 -net user \
 -smp 12 \
 -append "root=/dev/sda rw console=ttyS0,115200 acpi=off nokaslr" \
