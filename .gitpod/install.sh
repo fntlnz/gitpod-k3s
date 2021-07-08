b9df6e52d63a1a06c22e5d4f82c691b271aa1756
thisisommore <ommore501@gmail.com>
2021-07-08 10:09:43 +0000

    Install helm

--- .gitpod/install.sh
+++ .gitpod/install.sh
@@ -2,3 +2,4 @@ sudo apt update -y
 sudo apt install qemu qemu-system-x86 linux-image-$(uname -r) libguestfs-tools sshpass netcat -y
 sudo curl -o /usr/bin/kubectl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo chmod +x /usr/bin/kubectl
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
