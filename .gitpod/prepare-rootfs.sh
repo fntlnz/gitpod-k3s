35f938ddac9f02a854888b55e2922faf8f7d6381
thisisommore <ommore501@gmail.com>
2021-07-10 02:55:07 +0000

    prepare-rootfs: Add keys in rootfs

--- .gitpod/prepare-rootfs.sh
+++ .gitpod/prepare-rootfs.sh
@@ -34,7 +34,10 @@ sudo virt-customize -a bionic-server-cloudimg-amd64.img \
 --run-command "echo '${netconf}' > /etc/netplan/01-net.yaml" \
 --copy-in /lib/modules/5.4.0-1044-gke:/lib/modules \
 --run-command "sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config" \
---run-command "sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config"
+--run-command "sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config" \
+--run-command "ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key;
+               ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key;
+               ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key;"
 
 # mark as ready
 touch rootfs-ready.lock
